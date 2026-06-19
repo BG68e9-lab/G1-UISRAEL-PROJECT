981ddc4ae4d8c87a156d140fa70293abf740dc75
BryanGuerreroV <e1720856689@uisrael.edu.ec>
2026-06-18 22:34:55 -0500

    chore: Initial commit for project structure DrinkHouse

---
+++ src/main/java/com/uisrael/drinkhouse/DrinkhouseApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.drinkhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/drinkhouse/DrinkhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.drinkhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
