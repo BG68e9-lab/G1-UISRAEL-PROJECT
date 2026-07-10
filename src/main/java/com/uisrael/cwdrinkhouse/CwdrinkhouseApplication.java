b368eb7a13e44ceb653121bf0d4ceadfca6e8068
BryanGuerreroV <e1720856689@uisrael.edu.ec>
2026-07-10 13:07:58 -0500

    Initial Commit

---
+++ src/main/java/com/uisrael/cwdrinkhouse/CwdrinkhouseApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.cwdrinkhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwdrinkhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwdrinkhouseApplication.class, args);
+	}
+
+}
