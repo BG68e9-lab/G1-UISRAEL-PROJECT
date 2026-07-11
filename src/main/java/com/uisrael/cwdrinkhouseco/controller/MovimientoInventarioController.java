6128d74a3713699e89bbdddad8ef8e9c7ac96694
e1754198966-ops <e1754198966@uisrael.edu.ec>
2026-07-11 18:25:20 -0500

    adicion de controller en producto, proveedor y movimiento

---
+++ src/main/java/com/uisrael/cwdrinkhouseco/controller/MovimientoInventarioController.java
@@ -0,0 +1,22 @@
+package com.uisrael.cwdrinkhouseco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movimientoinventario")
+
+public class MovimientoInventarioController {
+	
+	
+	@GetMapping
+	public String leerPagina() {
+		
+		return "movimientosinventario/listarmovimientoinventario";
+	}
+
+	
+	
+
+}
